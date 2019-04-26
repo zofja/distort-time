51cf5ff319b336a71ab6eda5d2aa7625567cca61
Zofia Partyka <zofiapartyka1@gmail.com>
2019-04-26 14:58:25 +0200

    if_descentant function to check if pid is descended from current process

--- src/minix/servers/pm/distort_time.c
+++ src/minix/servers/pm/distort_time.c
@@ -3,12 +3,45 @@
 #include <minix/com.h>
 #include <signal.h>
 #include <sys/time.h>
+#include <stdbool.h>
 #include "mproc.h"
+
+
+bool if_descendant(pid_t pid) {
+
+  pid_t my_pid = mp->mp_pid; // get id of curr proc
+
+  struct mproc *lpid = find_proc(pid); // ptr to mproc of pid from parameter
+
+  while(lpid) {
+
+    printf("my pid %d   lpid %d\n", my_pid, lpid->mp_pid);
+    if (lpid->mp_pid == my_pid) {
+      return true;
+    }
+
+    lpid = &mproc[lpid->mp_parent];
+  }
+  return false;
+}
+
+bool if_ancestor(pid_t pid) {
+
+  return false;
+}
 
 int do_distort_time() {
 
   pid_t pid = m_in.m_distort_time.pid;
   uint8_t scale = m_in.m_distort_time.scale;
+
+  if (if_descendant(pid)) {
+    printf("is descendant\n");
+  } else if (if_ancestor(pid)) {
+    printf ("is ancesrot\n");
+  } else {
+    printf("is neither\n");
+  }
 
   printf("Hello from PM %d %d\n", pid, scale);
   return 0;
--- test_unistd_distort_time.c
+++ test_unistd_distort_time.c
@@ -1,8 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 int main(int argc, char** argv)
 {
-  distort_time(1, 2);
-  return 0;
+
+  pid_t pid;
+
+  switch (pid = fork()) {                     /* powstaje nowy proces */
+
+    case -1:                                  /* błąd funkcji fork() */
+      return -1;
+
+    case 0:                                   /* proces potomny */
+
+      printf("I am a child and my pid is %d\n", getpid());
+      printf("I am a child and fork() return value is %d\n", pid);
+
+      return 0;                               /* proces potomny kończy */
+
+    default:                                  /* proces macierzysty */
+
+      printf("I am a parent and my pid is %d\n", getpid());
+      printf("I am a parent and fork() return value is %d\n", pid);
+      distort_time(pid, 2);
+
+
+      if (wait(0) == -1) return -1;
+      /* czeka na zakończenie procesu potomnego */
+
+      return 0;
+  }
+
 }
